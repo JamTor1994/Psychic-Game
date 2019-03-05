5bc7da68cf4a725ac58833c9ec61c434542c672d
James.T <torresjames48@yahoo>
2019-03-05 01:05:25 -0600

    js fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,38 @@
-//alphabet set up
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
-'m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-// win lose var
-var wins= 0;
-var losses=0;
-var guess= 10;
-// computer choice set up
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(computerChoice)
+//var for letters
 
-//loging user inputs
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// tracking wins losse, and guesses
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+//computer choice
+
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    console.log(computerChoice)
+
+   // on key press function/user funct
 document.onkeypress = function(event) {
-    var userGues = event.key;
-// setting wins and loses
-els{
-    guess--;
-}
-if ( guesses === 0) {
-    losses ++
-}
+    var userGuess = event.key;
+
+    if(userGuess === computerChoice){
+        wins++;
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }else{
+        guesses--;
+        console.log(guesses)
+    }
+
+    if(guesses === 0){
+        losses++;
+        alert("You lose")
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)]
+    }
+//elements
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('losses').innerHTML = "losses: " + losses;
+    document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
 }
